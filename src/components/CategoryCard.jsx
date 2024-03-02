6846fece608d9250d76b24b49d7cb7fe9ecd3037
DiegoAaronMP <daaron.macias@gmail.com>
2024-03-01 18:27:51 -0600

    CategoryCard: Se optó por utilizar colores sólidos en lugar de gradientes.

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,11 +2,11 @@ import { IconBrandGithubFilled } from '@tabler/icons-react'
 
 export const CategoryCard = () => {
     return (
-        <div className='flex items-center justify-between rounded-xl hover:opacity-90 cursor-pointer bg-gradient-to-r from-cyan-500 to-blue-500'>
-            <div className='bg-white rounded-full p-4 my-auto'>
-                <IconBrandGithubFilled className='size-20 text-black' />
+        <div className='flex items-center justify-between rounded-xl text-black hover:text-white bg-slate-300 hover:bg-slate-800 cursor-pointer'>
+            <div className='p-4 my-auto'>
+                <IconBrandGithubFilled className='size-20' />
             </div>
-            <span className='mx-auto text-white font-roboto-condensed font-semibold text-2xl'>Categoría</span>
+            <span className='mx-auto font-roboto-condensed font-semibold text-2xl'>Categoría</span>
         </div>
     )
 }
