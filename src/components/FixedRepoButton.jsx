7da916b79b8fb25b61e167a9adf23dfc183624bd
DiegoAaronMP <daaron.macias@gmail.com>
2024-03-08 17:48:26 -0600

    Se realizaron mejoras en la semántica del sitio.

--- src/components/FixedRepoButton.jsx
+++ src/components/FixedRepoButton.jsx
@@ -3,8 +3,14 @@ import { IconBrandGithub } from '@tabler/icons-react'
 // TODO: Redireccionar a la página del repositorio
 export const FixedRepoButton = () => {
   return (
-    <button className='fixed group top-0 end-0 z-10 bg-black text-white py-1.5 px-5 rounded-bl-lg drop-shadow-sm'>
+    <nav>
+      <a  
+      className='fixed group top-0 end-0 z-10 bg-black text-white py-1.5 px-5 rounded-bl-lg drop-shadow-sm cursor-pointer'
+      target='_blank'
+      rel='noreferrer'
+      >
         <IconBrandGithub className='size-8 transition group-hover:scale-110' />
-    </button>
+      </a>
+    </nav>
   )
 }
--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -10,15 +10,15 @@ export const CategoriesPage = () => {
                 Catálogo de recursos para desarrolladores
             </h1>
             <h2 className='mt-4 mb-6 mx-auto text-center font-roboto-condensed text-xl'>
-                Un texto en <b className='text-amber-800'>negritas</b> como ejemplo
+                Un texto en <strong className='text-amber-800'>negritas</strong> como ejemplo
             </h2>
 
             {/* Contenedor de las cards*/}
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-max p-4 gap-3 gap-y-5 bg-white mx-7 md:mx-16 lg:mx-40 w-auto h-auto rounded-md'>
+            <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-max p-4 gap-3 gap-y-5 bg-white mx-7 md:mx-16 lg:mx-40 w-auto h-auto rounded-md'>
                 <CategoryCard />
                 <CategoryCard />
                 <CategoryCard />
-            </div>
+            </section>
         </>
     )
 }
