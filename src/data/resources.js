1a4321d630fe1b8c726e47de2d8dab1d4fd6e76a
DiegoAaronMP <daaron.macias@gmail.com>
2024-03-13 17:10:16 -0600

    Agregadas las URLs a los JSON de los recursos.

--- src/data/resources.js
+++ src/data/resources.js
@@ -5,7 +5,8 @@ export const resources = [
         "category": "Colores",
         "tags": ["Colores"],
         "description": "Descubre paletas de colores creadas por la comunidad.",
-        "image": "src/images/categories/colors/colorhunt.jpg"
+        "image": "src/images/categories/colors/colorhunt.jpg",
+        "url": "https://colorhunt.co/"
       },
       {
         "id": 2,
@@ -13,7 +14,8 @@ export const resources = [
         "category": "Colores",
         "tags": ["Colores", "Tailwind", "CSS", "SCSS", "Figma"],
         "description": "Genera paletas de colores para tu página web a partir de un color.",
-        "image": "src/images/categories/colors/uicolors.jpg"
+        "image": "src/images/categories/colors/uicolors.jpg",
+        "url": "https://uicolors.app/create"
       },
       {
         "id": 3,
@@ -21,7 +23,8 @@ export const resources = [
         "category": "Colores",
         "tags": ["Colores", "Accesibilidad"],
         "description": "Puntuación de la relación de contraste entre el color del texto y del fondo mediante inteligencia artificial.",
-        "image": "src/images/categories/colors/coolcontrast.jpg"
+        "image": "src/images/categories/colors/coolcontrast.jpg",
+        "url": "https://coolcontrast.vercel.app/"
       },
       {
         "id": 4,
@@ -29,7 +32,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["CSS"],
         "description": "Juego interactivo para aprender Grid.",
-        "image": "src/images/categories/education/gridgarden.jpg"
+        "image": "src/images/categories/education/gridgarden.jpg",
+        "url": "https://cssgridgarden.com/"
       },
       {
         "id": 5,
@@ -37,7 +41,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["CSS"],
         "description": "Juego interactivo para aprender Flexbox.",
-        "image": "src/images/categories/education/flexboxfroggy.jpg"
+        "image": "src/images/categories/education/flexboxfroggy.jpg",
+        "url": "https://flexboxfroggy.com/"
       },
       {
         "id": 6,
@@ -45,7 +50,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["JavaScript"],
         "description": "Un sitio con ideas para proyectos de JavaScript.",
-        "image": "src/images/categories/education/100proyectos.jpg"
+        "image": "src/images/categories/education/100proyectos.jpg",
+        "url": "https://www.javascript100.dev/"
       },
       {
         "id": 7,
@@ -53,7 +59,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["SQL"],
         "description": "Un sitio con preguntas técnicas de entrevistas sobre SQL y Ciencias de Datos.",
-        "image": "src/images/categories/education/datalemur.jpg"
+        "image": "src/images/categories/education/datalemur.jpg",
+        "url": "https://datalemur.com/"
       },
       {
         "id": 8,
@@ -61,7 +68,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["Git"],
         "description": "Juego interactivo para aprender Git.",
-        "image": "src/images/categories/education/ohmygit.jpg"
+        "image": "src/images/categories/education/ohmygit.jpg",
+        "url": "https://ohmygit.org/"
       },
       {
         "id": 9,
@@ -69,7 +77,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["JavaScript"],
         "description": "Juego interactivo para aprender JavaScript.",
-        "image": "src/images/categories/education/warriorjs.jpg"
+        "image": "src/images/categories/education/warriorjs.jpg",
+        "url": "https://warriorjs.com/"
       },
       {
         "id": 10,
@@ -77,7 +86,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["Documentación"],
         "description": "Colección de documentaciones de decenas de tecnologías.",
-        "image": "src/images/categories/education/devdocs.jpg"
+        "image": "src/images/categories/education/devdocs.jpg",
+        "url": "https://devdocs.io/"
       },
       {
         "id": 11,
@@ -85,7 +95,8 @@ export const resources = [
         "category": "Educación",
         "tags": ["Documentación"],
         "description": "Una recopilación de rutas de estudio para desarrolladores en diversas áreas.",
-        "image": "src/images/categories/education/roadmaps.jpg"
+        "image": "src/images/categories/education/roadmaps.jpg",
+        "url": "https://roadmap.sh/"
       },
       {
         "id": 12,
@@ -93,7 +104,8 @@ export const resources = [
         "category": "Fuentes",
         "tags": ["Fuentes"],
         "description": "Combinaciones de fuentes para tus diseños.",
-        "image": "src/images/categories/fonts/fontpair.jpg"
+        "image": "src/images/categories/fonts/fontpair.jpg",
+        "url": "https://www.fontpair.co/"
       },
       {
         "id": 13,
@@ -101,7 +113,8 @@ export const resources = [
         "category": "Fuentes",
         "tags": ["Fuentes"],
         "description": "Herramienta en línea para identificar fuentes de texto a partir de imágenes. ",
-        "image": "src/images/categories/fonts/whatfontis.jpg"
+        "image": "src/images/categories/fonts/whatfontis.jpg",
+        "url": "https://www.whatfontis.com/"
       },
       {
         "id": 14,
@@ -109,7 +122,8 @@ export const resources = [
         "category": "Fuentes",
         "tags": ["Fuentes"],
         "description": "Herramientas como el identificador de fuentes y el generador de fuentes web (webfonts).",
-        "image": "src/images/categories/fonts/fontsquirrel.jpg"
+        "image": "src/images/categories/fonts/fontsquirrel.jpg",
+        "url": "https://www.fontsquirrel.com/"
       },
       {
         "id": 15,
@@ -117,7 +131,8 @@ export const resources = [
         "category": "Iconos",
         "tags": ["SVG"],
         "description": "Más de 4950 íconos para diseño web.",
-        "image": "src/images/categories/icons/tabler.jpg"
+        "image": "src/images/categories/icons/tabler.jpg",
+        "url": "https://tabler.io/icons"
       },
       {
         "id": 16,
@@ -125,7 +140,8 @@ export const resources = [
         "category": "Iconos",
         "tags": ["SVG", "Ilustraciones"],
         "description": "Ilustraciones SVG personalizables.",
-        "image": "src/images/categories/icons/undraw.jpg"
+        "image": "src/images/categories/icons/undraw.jpg",
+        "url": "https://undraw.co/illustrations"
       },
       {
         "id": 17,
@@ -133,7 +149,8 @@ export const resources = [
         "category": "Herramientas",
         "tags": ["Imágenes"],
         "description": "Herramientas en línea para comprimir imágenes, convertirlas a JPG, eliminar fondos y más.",
-        "image": "src/images/categories/tools/iloveimg.jpg"
+        "image": "src/images/categories/tools/iloveimg.jpg",
+        "url": "https://www.iloveimg.com/"
       },
       {
         "id": 18,
@@ -141,7 +158,8 @@ export const resources = [
         "category": "Herramientas",
         "tags": ["Imágenes"],
         "description": "Aplicación web para comprimir y optimizar imágenes.",
-        "image": "src/images/categories/tools/squoosh.jpg"
+        "image": "src/images/categories/tools/squoosh.jpg",
+        "url": "https://squoosh.app/"
       },
       {
         "id": 19,
@@ -149,7 +167,8 @@ export const resources = [
         "category": "Herramientas",
         "tags": ["Colores, SVG, Iconos"],
         "description": "Colección de herramientas que incluye generadores de gradientes, compresión de SVG, conversión de SVG a JSX y más.",
-        "image": "src/images/categories/tools/omatsuri.jpg"
+        "image": "src/images/categories/tools/omatsuri.jpg",
+        "url": "https://omatsuri.app/"
       },
       {
         "id": 20,
@@ -157,6 +176,7 @@ export const resources = [
         "category": "Herramientas",
         "tags": ["JSON"],
         "description": "Genera datos en formato JSON de cualquier cosa mediante inteligencia artificial.",
-        "image": "src/images/categories/tools/jsondataai.jpg"
+        "image": "src/images/categories/tools/jsondataai.jpg",
+        "url": "https://www.jsondataai.com/"
       }
 ]
