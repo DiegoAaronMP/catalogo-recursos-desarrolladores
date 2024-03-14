b2aedfd3a9a3563080c4acbea71e31d11227a22d
DiegoAaronMP <daaron.macias@gmail.com>
2024-03-14 17:33:40 -0600

    mplementados iconos para las CategoryCard utilizando un objeto de objetos.

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,16 +1,19 @@
-import { IconBrandGithubFilled } from '@tabler/icons-react';
 import { categoryStyles } from '../consts/categoryStyles';
+import { categoryIcons } from '../consts/categoryIcons';
 
 export const CategoryCard = ({title}) => {
 
     // Colores para las cards
     const {bgColorClass, textColorClass, hoverBgColorClass, hoverTextColorClass,} = categoryStyles[title] || {};
 
+    // Iconos para las cards
+    const {Icon} = categoryIcons[title] || {};
+
     return (
         <a href={`/${title}`} className={`flex items-center justify-between rounded-xl transition ${bgColorClass} ${textColorClass} ${hoverBgColorClass} ${hoverTextColorClass} active:brightness-150 cursor-pointer`} rel='noreferrer'>
             <span className='mx-auto font-roboto-condensed font-semibold text-2xl'>{title}</span>
             <div className='p-4 my-auto'>
-                <IconBrandGithubFilled className='size-20' />
+                <Icon className='size-20' />
             </div>
         </a>
     )
---
+++ src/consts/categoryIcons.js
@@ -0,0 +1,9 @@
+import { IconIcons, IconPalette, IconTools, IconLetterCase, IconSchool } from "@tabler/icons-react";
+
+export const categoryIcons = {
+    Iconos: {Icon: IconIcons},
+    Colores: { Icon: IconPalette },
+    Herramientas: { Icon: IconTools },
+    Fuentes: { Icon: IconLetterCase },
+    Educación: { Icon: IconSchool },
+};
