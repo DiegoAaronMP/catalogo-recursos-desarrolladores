35336a39c829dfb7193ede4b441232e49177962c
DiegoAaronMP <daaron.macias@gmail.com>
2024-03-21 18:37:16 -0600

    Mejorada la accesibilidad de CategoryCard.

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,19 +1,26 @@
 import { categoryStyles } from '../consts/categoryStyles';
 import { categoryIcons } from '../consts/categoryIcons';
 
-export const CategoryCard = ({title}) => {
+export const CategoryCard = ({ title }) => {
 
     // Colores para las cards
-    const {bgColorClass, textColorClass, hoverBgColorClass, hoverTextColorClass,} = categoryStyles[title] || {};
+    const { bgColorClass, textColorClass, hoverBgColorClass, hoverTextColorClass, } = categoryStyles[title] || {};
 
     // Iconos para las cards
-    const {Icon} = categoryIcons[title] || {};
+    const { Icon } = categoryIcons[title] || {};
 
     return (
-        <a href={`/${title}`} className={`flex items-center justify-between rounded-xl transition ${bgColorClass} ${textColorClass} ${hoverBgColorClass} ${hoverTextColorClass} active:brightness-150 cursor-pointer`} rel='noreferrer'>
+        <a
+            href={`/${title}`}
+            className={`flex items-center justify-between rounded-xl transition ${bgColorClass} ${textColorClass} ${hoverBgColorClass} ${hoverTextColorClass} active:brightness-150 cursor-pointer`}
+            rel='noreferrer'
+            role='button'
+            aria-label={`Ir a la página de ${title}`}
+            title={`Ir a la página de ${title}`}
+        >
             <span className='mx-auto font-roboto-condensed font-semibold text-2xl'>{title}</span>
             <div className='p-4 my-auto'>
-                <Icon className='size-20' />
+                <Icon className='size-20' alt={`Ícono de ${title}`} />
             </div>
         </a>
     )
