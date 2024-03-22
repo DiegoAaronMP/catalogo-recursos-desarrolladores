6c335b9cf188eeb4cf91594d6f7ff0c4de666123
DiegoAaronMP <daaron.macias@gmail.com>
2024-03-21 18:31:32 -0600

    Mejorada la accesibilidad de FixedRepoButton.

--- src/components/FixedRepoButton.jsx
+++ src/components/FixedRepoButton.jsx
@@ -4,12 +4,15 @@ export const FixedRepoButton = () => {
   return (
     <nav>
       <a  
-      className='fixed group top-0 end-0 z-10 bg-black text-white py-1.5 px-5 rounded-bl-lg drop-shadow-sm cursor-pointer'
-      href='https://github.com/DiegoAaronMP/catalogo-recursos-desarrolladores'
-      target='_blank'
-      rel='noreferrer'
+        className='fixed group top-0 end-0 z-10 bg-black text-white py-1.5 px-5 rounded-bl-lg drop-shadow-sm cursor-pointer'
+        href='https://github.com/DiegoAaronMP/catalogo-recursos-desarrolladores'
+        role='button'
+        aria-label='Ir a la página del repositorio en Github'
+        title='Ir a la página del repositorio en Github'
+        target='_blank'
+        rel='noreferrer'
       >
-        <IconBrandGithub className='size-8 transition group-hover:scale-110' />
+        <IconBrandGithub className='size-8 transition group-hover:scale-110' alt='Ícono de GitHub'/>
       </a>
     </nav>
   )
